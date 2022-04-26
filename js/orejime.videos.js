bd55e97b29f00adcf1dbecfbc5518872287f9a3d
Matthias Andriessen <matthias.andriessen@intracto.com>
2022-04-26 09:42:11 +0200

    only loading when accepted cookie

--- js/orejime.videos.js
+++ js/orejime.videos.js
@@ -1,1 +1,28 @@
-console.log('loaded');
+(function ($) {
+
+  $(document).ready(function() {
+    
+    tryToLoadContent();
+    
+    $("#orejime-loadconsent").on("click", function() {
+      orejime.show();
+    });
+
+    $(".orejime-Button--save").bind("click", function() {
+      tryToLoadContent();
+    });
+    
+    function tryToLoadContent() {
+
+      let socialmedia = orejime.internals.manager.states.socialmedia;
+      
+      if(socialmedia) {      
+        contents = $('.embed').html();
+        $('.orejime-embed.placeholder .content').html(contents);
+      }
+    }
+  
+  });
+  
+
+}(jQuery));
