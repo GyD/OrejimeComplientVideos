886ba1058356eb5579f1755710aa027466f87483
Matthias Andriessen <matthias@ohhert.be>
2022-05-20 11:21:30 +0200

    Cookie consent same dimensions as embed

--- assets/js/orejime.videos.js
+++ assets/js/orejime.videos.js
@@ -1,41 +1,47 @@
-(function ($) {
-
+;(function ($) {
   $(document).ready(function () {
-
-    tryToLoadContent();
+    changeWidthOfPlacholder()
+    tryToLoadContent()
 
     $("#orejime-loadconsent").on("click", function (e) {
-      e.preventDefault();
-      orejime.show();
-    });
+      e.preventDefault()
+      orejime.show()
+    })
 
-    $(document).on('click', ".orejime-Button--save", function () {
-      tryToLoadContent();
-    });
+    $(document).on("click", ".orejime-Button--save", function () {
+      tryToLoadContent()
+    })
 
     /**
-     *
+     * Check if the social media cookie is enabled, and load content if enabled
      */
     function tryToLoadContent() {
-
       // let socialmedia = orejime.internals.manager.states.socialmedia;
-      let socialmedia = orejime.internals.manager.getConsent(drupalSettings.orejime_videos.consent);
+      let socialmedia = orejime.internals.manager.getConsent(
+        drupalSettings.orejime_videos.consent
+      )
       if (socialmedia) {
-
         $(".orejime_template").each(function () {
-          const
-            embededID = $(this).data('orejime-embeded-id'),
-            content = $(this).html()
-          ;
+          const EMBEDEDID = $(this).data("orejime-embeded-id"),
+            CONTENT = $(this).html()
 
-          $(".orejime_embed.placeholder[data-orejime-embeded-id='" + embededID + "'] .orejime_content").html(content);
-
-        });
-
+          $(
+            ".orejime_placeholder[data-orejime-embeded-id='" +
+              EMBEDEDID +
+              "'] .orejime_embed"
+          ).replaceWith(CONTENT)
+        })
       }
     }
 
-  });
-
-
-}(jQuery));
+    /**
+     * Change the width of the placeholder based on the width-attribute
+     */
+    function changeWidthOfPlacholder() {
+      $(".orejime_placeholder").each(function () {
+        const WIDTH = $(this).attr("width")
+        $(this).width(WIDTH)
+      })
+    }
+  })
+})(jQuery)
