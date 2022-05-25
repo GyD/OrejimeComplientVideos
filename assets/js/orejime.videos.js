fa269403df95b15e017eb8b670644454ff597990
Yves Guide <yves@blue4you.be>
2022-05-25 12:09:53 +0200

    Fixed ESLint & JSHint errors

--- assets/js/orejime.videos.js
+++ assets/js/orejime.videos.js
@@ -1,45 +1,51 @@
-;(function ($) {
+/* global orejime:readonly */
+(function ($) {
+  /**
+   * Check if the social media cookie is enabled, and load content if enabled
+   */
+  function tryToLoadContent() {
+    // eslint-disable-next-line func-names
+    $(".orejime_template").each(function () {
+      const domain = $(this).attr("data-orejime-domain");
+
+      if (!orejime.internals.manager.getConsent(domain)) {
+        return;
+      }
+
+      const embededId = $(this).data("orejime-embeded-id");
+      const content = $(this).html();
+
+      $(
+        `.orejime_placeholder[data-orejime-embeded-id="${embededId}"] .orejime_embed`
+      ).replaceWith(content);
+    });
+  }
+
+  /**
+   * Change the width of the placeholder based on the width-attribute
+   */
+  function changeWidthOfPlacholder() {
+    // eslint-disable-next-line func-names
+    $(".orejime_placeholder").each(function () {
+      const width = $(this).attr("width");
+      $(this).width(width);
+    });
+  }
+
+  // eslint-disable-next-line func-names
   $(document).ready(function () {
-    changeWidthOfPlacholder()
-    tryToLoadContent()
+    changeWidthOfPlacholder();
+    tryToLoadContent();
 
+    // eslint-disable-next-line func-names
     $(document).on("click", ".orejime_loadconsent", function (e) {
-      e.preventDefault()
-      orejime.show()
-    })
+      e.preventDefault();
+      orejime.show();
+    });
 
+    // eslint-disable-next-line func-names
     $(document).on("click", ".orejime-Button--save", function () {
-      tryToLoadContent()
-    })
-
-    /**
-     * Check if the social media cookie is enabled, and load content if enabled
-     */
-    function tryToLoadContent() {
-      $(".orejime_template").each(function () {
-        const domain = $(this).attr("data-orejime-domain");
-
-        if (!orejime.internals.manager.getConsent(domain)) {
-          return;
-        }
-
-        const embeded_id = $(this).data("orejime-embeded-id"),
-          content = $(this).html()
-        ;
-
-        $(".orejime_placeholder[data-orejime-embeded-id='" + embeded_id + "'] .orejime_embed").replaceWith(content)
-      })
-
-    }
-
-    /**
-     * Change the width of the placeholder based on the width-attribute
-     */
-    function changeWidthOfPlacholder() {
-      $(".orejime_placeholder").each(function () {
-        const WIDTH = $(this).attr("width")
-        $(this).width(WIDTH)
-      })
-    }
-  })
-})(jQuery)
+      tryToLoadContent();
+    });
+  });
+})(jQuery);
